c58c9338bbb563cd149b7add1738e5dcd93d121b
Lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-05-29 19:14:21 +0000

    feat: design mobile layout

--- src/components/forecastItem/ForecastItem.jsx
+++ src/components/forecastItem/ForecastItem.jsx
@@ -6,7 +6,9 @@ const ForecastItem = ({high, low, iconURL, day}) =>{
     return(
         <div className={styles.forecast__item}>
             <h3>{day}</h3>
-            <img src={`${iconURL}`} alt="weather icon"/>
+            <div className={styles.icon_container}>
+                <img src={`${iconURL}`} alt="weather icon" className={styles.icon}/>
+            </div>
             <div className={styles.high_low}>
                 <p>{high}&deg;</p>
                 <p>{low}&deg;</p>
--- src/components/weatherDisplay/WeatherDisplay.jsx
+++ src/components/weatherDisplay/WeatherDisplay.jsx
@@ -3,21 +3,14 @@ import React from "react";
 import styles from "./WeatherDisplay.module.scss";
 
 import TodayWeatherContainer from '../../containers/todayWeatherContainer';
-import ForecastItem from "../forecastItem/ForecastItem";
+import ForecastContainer from '../../containers/ForecastContainer';
 
 const WeatherDisplay = () => {
     return(
         <div className={styles.weatherDisplay}>
             <h2>Delta, CO</h2>
             <TodayWeatherContainer />
-            <div className={styles.forecast}>
-                <ForecastItem 
-                    high={50}
-                    low={30}
-                    iconURL={"https://openweathermap.org/img/wn/10d@2x.png"}
-                    day="Tuesday"
-                />
-            </div>
+            <ForecastContainer />
         </div>
     );
 };
